000f62c7a2a30218c504d32ff60d603cd23319f9
nandakumarpurohit <nandakumarpurohit@gmail.com>
2019-01-18 11:44:50 -0500

    init files

---
+++ spring-security-jdbc-authentication-example/tables.sql
@@ -0,0 +1,23 @@
+create table users(
+	username varchar(50) not null primary key,
+	password varchar(100) not null,
+	enabled boolean not null
+);
+create table authorities (
+	username varchar(50) not null,
+	authority varchar(50) not null,
+	constraint fk_authorities_users foreign key(username) references users(username)
+);
+create unique index ix_auth_username on authorities (username,authority);
+
+insert into users(username,password,enabled)
+	values('admin','$2a$10$hbxecwitQQ.dDT4JOFzQAulNySFwEpaFLw38jda6Td.Y/cOiRzDFu',true);
+insert into authorities(username,authority) 
+	values('admin','ROLE_ADMIN');
+	
+/*
+Before inserting data into tables, you can encrypt the password using the BCryptPasswordEncoder. 
+
+String encoded=new BCryptPasswordEncoder().encode("admin@123");
+System.out.println(encoded);		
+*/
