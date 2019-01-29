464e9080ebed05d3a85dd2fefa63f3d52e9a8b27
nandakumarpurohit <nandakumarpurohit@gmail.com>
2019-01-29 15:26:26 -0500

    init files

---
+++ boot-security-custom-login-page/src/main/java/com/demo/service/EmployeeService.java
@@ -0,0 +1,12 @@
+package com.demo.service;
+
+import java.util.List;
+
+import com.demo.model.Employee;
+
+public interface EmployeeService {
+	void insertEmployee(Employee emp);
+	void insertEmployees(List<Employee> employees);
+	List<Employee> getAllEmployees();
+	void getEmployeeById(String empid);
+}
