f65fa845bb64eb5b164c8dd32aaa268e41328b29
nandakumarpurohit <nandakumarpurohit@gmail.com>
2019-01-29 15:41:40 -0500

    init files

---
+++ boot-security-jdbc-authentication/src/main/java/com/demo/dao/EmployeeDao.java
@@ -0,0 +1,12 @@
+package com.demo.dao;
+
+import java.util.List;
+
+import com.demo.model.Employee;
+
+public interface EmployeeDao {
+	void insertEmployee(Employee cus);
+	void insertEmployees(List<Employee> employees);
+	List<Employee> getAllEmployees();
+	Employee getEmployeeById(String empId);
+}
---
+++ boot-security-jdbc-authentication/src/main/java/com/demo/service/impl/EmployeeServiceImpl.java
@@ -0,0 +1,38 @@
+package com.demo.service.impl;
+
+import java.util.List;
+
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.stereotype.Service;
+
+import com.demo.dao.EmployeeDao;
+import com.demo.model.Employee;
+import com.demo.service.EmployeeService;
+
+@Service
+public class EmployeeServiceImpl implements EmployeeService {
+
+	@Autowired
+	EmployeeDao employeeDao;
+
+	@Override
+	public void insertEmployee(Employee employee) {
+		employeeDao.insertEmployee(employee);
+	}
+
+	@Override
+	public void insertEmployees(List<Employee> employees) {
+		employeeDao.insertEmployees(employees);
+	}
+
+	public List<Employee> getAllEmployees() {
+		return employeeDao.getAllEmployees();
+	}
+
+	@Override
+	public void getEmployeeById(String empId) {
+		Employee employee = employeeDao.getEmployeeById(empId);
+		System.out.println(employee);
+	}
+
+}
